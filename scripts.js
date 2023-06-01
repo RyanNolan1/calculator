c37fb5fc175274c60862887981d9d62c5e97e036
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-06-01 12:09:23 +0100

    this will add the operate function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(operator, firstNumber, secondNumber) {
+  if (operator === "+") {
+    add(firstNumber, secondNumber);
+  } else if (operator === "-") {
+    subtract(firstNumber, secondNumber);
+  } else if (operator === "*") {
+    multiply(firstNumber, secondNumber);
+  } else if (operator === "/") {
+    divide(firstNumber, secondNumber);
+  }
+}
+
 function add(a, b) {
   return a + b;
 }
