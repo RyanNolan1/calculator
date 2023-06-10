37ff8f106154bd7d720bc018dfdaceb9de1c4c37
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-06-10 22:36:36 +0100

    this will get the initial calculations for 2 numbers logging to console

--- scripts.js
+++ scripts.js
@@ -2,48 +2,49 @@ const numberButtons = document.getElementsByClassName("number-buttons");
 const calculatorScreen = document.getElementById("calculator-screen");
 const operatorButtons = document.getElementsByClassName("operator-buttons");
 const clearButton = document.getElementById("ac-button");
+const equalsButton = document.getElementById("equals-button");
 
-let displayValue = 0;
-let firstNumber;
-let operator;
-let secondNumber;
-
-clearButton.addEventListener("click", function () {
-displayValue = 0;
-calculatorScreen.innerText = displayValue;
-})
-
-for (let i = 0; i < operatorButtons.length; i++) {
-  
-  const operatorButton = operatorButtons[i];
-  operatorButton.addEventListener("click", function () {
-    displayValue = "";
-    const buttonOperator = operatorButton.textContent;
-    firstNumber = Number(displayValue); //  first number on screen when the number is entered
-    operator = buttonOperator; //  the + / * symbol
-  });
-}
+let displayValue = "";
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
 
 for (let i = 0; i < numberButtons.length; i++) {
-  displayValue = "";
   const numberButton = numberButtons[i];
   numberButton.addEventListener("click", function () {
     const buttonNumber = numberButton.textContent;
-    displayValue = displayValue + buttonNumber;
+    displayValue += buttonNumber;
     calculatorScreen.innerText = displayValue;
     return displayValue;
   });
 }
 
+for (let i = 0; i < operatorButtons.length; i++) {
+  const operatorButton = operatorButtons[i];
+  operatorButton.addEventListener("click", function () {
+    const buttonOperator = operatorButton.textContent;
+    firstNumber = Number(displayValue); //  first number on screen when the number is entered
+    operator = buttonOperator; //  the + / * symbol
+    displayValue = "";
+    console.log(firstNumber);
+    console.log(operator);
+  });
+}
+
+equalsButton.addEventListener("click", function () {
+  secondNumber = Number(displayValue);
+  operate(operator, firstNumber, secondNumber);
+});
+
 function operate(operator, firstNumber, secondNumber) {
   if (operator === "+") {
-    add(firstNumber, secondNumber);
+    console.log(add(firstNumber, secondNumber));
   } else if (operator === "-") {
-    subtract(firstNumber, secondNumber);
-  } else if (operator === "*") {
-    multiply(firstNumber, secondNumber);
-  } else if (operator === "/") {
-    divide(firstNumber, secondNumber);
+    console.log(subtract(firstNumber, secondNumber));
+  } else if (operator === "x") {
+    console.log(multiply(firstNumber, secondNumber));
+  } else if (operator === "÷") {
+    console.log(divide(firstNumber, secondNumber));
   }
 }
 
