c46bc4045f060b968c6b6a7967746f0eac5e1c2a
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-06-11 21:34:40 +0100

    this will allow the calculator to do multiple calculations

--- scripts.js
+++ scripts.js
@@ -15,7 +15,6 @@ for (let i = 0; i < numberButtons.length; i++) {
     const buttonNumber = numberButton.textContent;
     displayValue += buttonNumber;
     calculatorScreen.innerText = displayValue;
-    return displayValue;
   });
 }
 
@@ -23,28 +22,27 @@ for (let i = 0; i < operatorButtons.length; i++) {
   const operatorButton = operatorButtons[i];
   operatorButton.addEventListener("click", function () {
     const buttonOperator = operatorButton.textContent;
-    firstNumber = Number(displayValue); //  first number on screen when the number is entered
-    operator = buttonOperator; //  the + / * symbol
+    firstNumber = Number(calculatorScreen.innerText);
+    operator = buttonOperator; 
     displayValue = "";
-    console.log(firstNumber);
-    console.log(operator);
   });
 }
 
 equalsButton.addEventListener("click", function () {
   secondNumber = Number(displayValue);
+  displayValue = "";
   operate(operator, firstNumber, secondNumber);
 });
 
 function operate(operator, firstNumber, secondNumber) {
   if (operator === "+") {
-    console.log(add(firstNumber, secondNumber));
+    calculatorScreen.innerText = add(firstNumber, secondNumber);
   } else if (operator === "-") {
-    console.log(subtract(firstNumber, secondNumber));
+    calculatorScreen.innerText = subtract(firstNumber, secondNumber);
   } else if (operator === "x") {
-    console.log(multiply(firstNumber, secondNumber));
+    calculatorScreen.innerText = multiply(firstNumber, secondNumber);
   } else if (operator === "÷") {
-    console.log(divide(firstNumber, secondNumber));
+    calculatorScreen.innerText = divide(firstNumber, secondNumber);
   }
 }
 
