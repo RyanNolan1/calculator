235a8d3ed1e6367db3b2d5ffb2cc3caad1dfd1c5
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-06-22 07:45:38 +0100

    this will make the input to the functions as strings and then convert them to nunmbers

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-const numberButtons = document.getElementsByClassName("number-buttons");
+const numberButtons = document.querySelectorAll(".number-buttons");
 const calculatorScreen = document.getElementById("calculator-screen");
-const operatorButtons = document.getElementsByClassName("operator-buttons");
+const operatorButtons = document.querySelectorAll(".operator-buttons");
 const clearButton = document.getElementById("ac-button");
 const equalsButton = document.getElementById("equals-button");
 const backButton = document.getElementById("back-button");
 
-let displayValue = "";
+let inputtedValue = "";
+let calculationArray = [];
 let firstNumber = 0;
 let operator = "";
 let secondNumber = 0;
-let calculationArray = [];
 
 for (let i = 0; i < numberButtons.length; i++) {
   const numberButton = numberButtons[i];
   numberButton.addEventListener("click", function () {
-    const buttonNumber = numberButton.textContent;
-    displayValue += buttonNumber;
-    calculatorScreen.innerText = displayValue;
+    const buttonValue = numberButton.textContent;
+    inputtedValue += buttonValue;
+    calculatorScreen.innerText = inputtedValue;
   });
 }
 
@@ -24,44 +24,55 @@ for (let i = 0; i < operatorButtons.length; i++) {
   const operatorButton = operatorButtons[i];
   operatorButton.addEventListener("click", function () {
     const buttonOperator = operatorButton.textContent;
-    firstNumber = Number(calculatorScreen.innerText);
-    operator = buttonOperator;
-    displayValue = "";
-    calculationArray.push(firstNumber, operator);
+    calculationArray.push(inputtedValue, buttonOperator);
+    inputtedValue = "";
 
-    if (calculationArray.length > 2) {
+    if (calculationArray.length === 4 ) {
+      console.log(calculationArray)
       firstNumber = calculationArray[0];
       operator = calculationArray[1];
       secondNumber = calculationArray[2];
       calculatorScreen.innerText = operate(operator, firstNumber, secondNumber);
       calculationArray.splice(0, 3);
-      calculationArray.unshift(Number(calculatorScreen.innerText));
+      calculationArray.unshift(calculatorScreen.innerText);
     }
-  });
+  })
 }
 
+
+
+  
+  
+  // for (let i = 0; i < operatorButtons.length; i++) {
+    //   const operatorButton = operatorButtons[i];
+    //   operatorButton.addEventListener("click", function () {
+//     const buttonOperator = operatorButton.textContent;
+//     firstNumber = Number(calculatorScreen.innerText);
+//     operator = buttonOperator;
+//     displayValue = "";
+//     calculationArray.push(firstNumber, operator);
+
 equalsButton.addEventListener("click", function () {
-  firstNumber = Number(calculationArray[0]);
-  secondNumber = Number(displayValue);
-  displayValue = "";
+  firstNumber = calculationArray[0];
+  operator = calculationArray[1]
+  secondNumber = inputtedValue;
   calculatorScreen.innerText = operate(operator, firstNumber, secondNumber);
-  calculationArray = [];
 });
 
-clearButton.addEventListener("click", function () {
-  calculationArray = [];
-  calculatorScreen.innerHTML = 0;
-});
+// clearButton.addEventListener("click", function () {
+//   calculationArray = [];
+//   calculatorScreen.innerHTML = 0;
+// });
 
-backButton.addEventListener("click", function () {
-  calculatorScreen.innerHTML = calculatorScreen.innerHTML.replace(/.$/, "");
-});
+// backButton.addEventListener("click", function () {
+//   calculatorScreen.innerHTML = calculatorScreen.innerHTML.replace(/.$/, "");
+// });
 
 function operate(operator, firstNumber, secondNumber) {
-  if (operator === "+") return add(firstNumber, secondNumber);
-  if (operator === "-") return subtract(firstNumber, secondNumber);
-  if (operator === "x") return multiply(firstNumber, secondNumber);
-  if (operator === "÷") return divide(firstNumber, secondNumber);
+  if (operator === "+") return add(Number(firstNumber), Number(secondNumber));
+  if (operator === "-") return subtract(Number(firstNumber), Number(secondNumber));
+  if (operator === "x") return multiply(Number(firstNumber), Number(secondNumber));
+  if (operator === "÷") return divide(Number(firstNumber), Number(secondNumber));
 }
 
 function add(a, b) {
@@ -79,3 +90,4 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
